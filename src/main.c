42b86852db26c9338f663e7a7241c38b7c0300a6
AngryBeltMan <groupydrone@gmail.com>
2023-08-29 17:30:25 -0400

    updated

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main(void) {
 
     /* Initialize the tilemap structure */
     unsigned char* map = malloc(18 * 14);
-    memset(map, TileType_Transparent, 18 * 14);
+    memset(map, TileType_SnakeHead, 18 * 14);
     tilemap.map = map;
     tilemap.tiles = tileset_tiles;
     tilemap.type_width = 18;
@@ -37,15 +37,13 @@ int main(void) {
     Snake snake = SNAKE_init();
 
     gfx_Begin();
-    gfx_SetTransparentColor(0);
     /* gfx_SetDrawBuffer(); */
 
     gfx_SetPalette(global_palette, sizeof_global_palette, 0);
     zx7_Decompress(gfx_vram, growmi_bg_compressed);
+    gfx_SetTransparentColor(0);
     do {
         gfx_Tilemap_NoClip(&tilemap, x_offset, y_offset);
-        gfx_SetColor(0);
-        gfx_FillRectangle(50,50,100,100);
         /* gfx_FillScreen(32); */
         // clear the screen
         /* SNAKE_move(&snake); */
